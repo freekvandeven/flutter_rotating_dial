29276eacff6ecb81549fc07fe5778a23447ab623
Freek van de Ven <freek@iconica.nl>
2022-06-17 08:49:51 +0200

    hello world widget content

---
+++ lib/rotating_dial_widget.dart
@@ -0,0 +1,5 @@
+library rotating_dial_widget;
+
+import 'package:flutter/material.dart';
+
+part 'src/rotating_dial.dart';
---
+++ lib/src/rotating_dial.dart
@@ -0,0 +1,15 @@
+part of rotating_dial_widget;
+
+class RotatingDialWidget extends StatefulWidget {
+  const RotatingDialWidget({Key? key}) : super(key: key);
+
+  @override
+  State<RotatingDialWidget> createState() => _RotatingDialWidgetState();
+}
+
+class _RotatingDialWidgetState extends State<RotatingDialWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello World');
+  }
+}
