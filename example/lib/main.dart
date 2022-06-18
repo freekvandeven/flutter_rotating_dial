8cce3fd1bcdf2af755d1b8d53a72e6aa57beefd2
Freek van de Ven <FJM.vandeVen@student.han.nl>
2022-06-18 11:44:29 +0200

    fully configured dial example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,7 +43,67 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RotatingDialWidget(),
+            SizedBox(
+              width: 400,
+              height: 400,
+              child: RotatingDialWidget(
+                title: 'Average',
+                titleColor: const Color(0xFF000000),
+                dialColor: const Color(0xFF1164e9),
+                markerColor: const Color(0xFFffca5c),
+                textColor: const Color(0xFFffffff),
+                sections: [
+                  RotatingDialSection(
+                    label: '18-29',
+                    value: '2:00:00',
+                    ringPercentage: 0.50,
+                    ringColor: Colors.blue,
+                  ),
+                  RotatingDialSection(
+                    label: '30-39',
+                    value: '3:00:00',
+                    ringPercentage: 0.10,
+                    ringColor: Colors.green,
+                  ),
+                  RotatingDialSection(
+                    label: '40-49',
+                    value: '4:00:00',
+                    ringPercentage: 0.10,
+                    ringColor: Colors.yellow,
+                  ),
+                  RotatingDialSection(
+                    label: '50-54',
+                    value: '5:00:00',
+                    ringPercentage: 0.10,
+                    ringColor: Colors.orange,
+                  ),
+                  RotatingDialSection(
+                    label: '55-59',
+                    value: '6:00:00',
+                    ringPercentage: 0.5,
+                    ringColor: Colors.red,
+                  ),
+                  RotatingDialSection(
+                    label: '60-64',
+                    value: '7:00:00',
+                    ringPercentage: 0.5,
+                    ringColor: Colors.pink,
+                  ),
+                  RotatingDialSection(
+                    label: '65-69',
+                    value: '8:00:00',
+                    ringPercentage: 0.5,
+                    ringColor: Colors.purple,
+                  ),
+                  RotatingDialSection(
+                    label: '>69',
+                    value: '9:00:00',
+                    ringPercentage: 0.5,
+                    ringColor: Colors.brown,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
